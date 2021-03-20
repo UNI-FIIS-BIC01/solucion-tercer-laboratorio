ba6b222a5694a9b4b312010dc4ff0f4dbb841fa8
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-03-20 13:33:18 +0000

    Solución al tercer laboratorio

--- ahorcado.py
+++ ahorcado.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 ARCHIVO_DICCIONARIO = "diccionario.txt"
 
@@ -34,8 +35,12 @@ def palabra_completada(palabra_secreta, letras_seleccionadas):
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
 
+    for letra in palabra_secreta:
+        if letra not in letras_seleccionadas:
+            return False
+
+    return True
     # FIN
-    return
 
 
 def obtener_solucion_como_cadena(palabra_secreta, letras_seleccionadas):
@@ -49,9 +54,16 @@ def obtener_solucion_como_cadena(palabra_secreta, letras_seleccionadas):
     """
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
+    solucion_como_cadena = ""
 
+    for letra in palabra_secreta:
+        if letra in letras_seleccionadas:
+            solucion_como_cadena += letra
+        else:
+            solucion_como_cadena += "_ "
+
+    return solucion_como_cadena
     # FIN
-    return
 
 
 def obtener_letras_disponibles(letras_seleccionadas):
@@ -62,9 +74,90 @@ def obtener_letras_disponibles(letras_seleccionadas):
     """
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
+    letras_disponibles = ""
+    for letra in string.ascii_lowercase:
+        if letra not in letras_seleccionadas:
+            letras_disponibles += letra
 
+    return letras_disponibles
     # FIN
-    return
+
+
+def error_con_advertencia(oportunidades, advertencias, razon, solucion_como_cadena):
+    if advertencias >= 1:
+        advertencias -= 1
+    else:
+        oportunidades -= 1
+
+    print("Error! " + razon + ". Te quedan " + str(advertencias) + " advertencias: " +
+          solucion_como_cadena)
+
+    return oportunidades, advertencias
+
+
+def mostrar_ganador(oportunidades, palabra_secreta):
+    print("Felicitaciones, ganaste!")
+
+    letras_unicas = []
+    for letra in palabra_secreta:
+        if letra not in letras_unicas:
+            letras_unicas.append(letra)
+
+    puntaje = oportunidades * len(letras_unicas)
+    print("El puntaje obtenido es " + str(puntaje))
+
+
+def error_letra_incorrecta(penalidad, oportunidades, solucion_como_cadena):
+    oportunidades -= penalidad
+    print("Error! Esa letra no esta en la palabra: " + solucion_como_cadena)
+    return oportunidades
+
+
+def iniciar_ahorcado(palabra_secreta, con_ayuda):
+    print("Este es el juego del ahorcado!")
+    print("Adivina el pais. Tiene " + str(len(palabra_secreta)) + " letras.")
+    print("-----------")
+
+    letras_seleccionadas = []
+    oportunidades = 6
+    advertencias = 3
+
+    while not palabra_completada(palabra_secreta, letras_seleccionadas) and oportunidades >= 0:
+        print("Te quedan " + str(oportunidades) + " oportunidades.")
+        print("Letras disponibles: " + obtener_letras_disponibles(letras_seleccionadas))
+
+        letra = input("Ingresa una letra: ").lower()
+
+        if con_ayuda and letra == "*":
+            print("Posibles candidatos:")
+            mostrar_candidatos(obtener_solucion_como_cadena(palabra_secreta,
+                                                            letras_seleccionadas))
+        elif not letra.isalpha():
+            oportunidades, advertencias = error_con_advertencia(oportunidades, advertencias, "No es una letra valida",
+                                                                obtener_solucion_como_cadena(palabra_secreta,
+                                                                                             letras_seleccionadas))
+        elif letra in letras_seleccionadas:
+            oportunidades, advertencias = error_con_advertencia(oportunidades, advertencias,
+                                                                "Ya has ingresado esa letra",
+                                                                obtener_solucion_como_cadena(palabra_secreta,
+                                                                                             letras_seleccionadas))
+        elif letra in palabra_secreta:
+            letras_seleccionadas.append(letra)
+            print("Exito!: " + obtener_solucion_como_cadena(palabra_secreta, letras_seleccionadas))
+
+        elif letra not in palabra_secreta and letra in "aeiou":
+            letras_seleccionadas.append(letra)
+            oportunidades = error_letra_incorrecta(2, oportunidades, obtener_solucion_como_cadena(palabra_secreta,
+                                                                                                  letras_seleccionadas))
+
+        elif letra not in palabra_secreta and letra not in "aeiou":
+            letras_seleccionadas.append(letra)
+            oportunidades = error_letra_incorrecta(1, oportunidades, obtener_solucion_como_cadena(palabra_secreta,
+                                                                                                  letras_seleccionadas))
+    if palabra_completada(palabra_secreta, letras_seleccionadas):
+        mostrar_ganador(oportunidades, palabra_secreta)
+    else:
+        print("Lo siento, te quedaste sin oportunidades. El pais era " + palabra_secreta)
 
 
 def iniciar(palabra_secreta):
@@ -87,9 +180,8 @@ def iniciar(palabra_secreta):
     """
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
-
+    iniciar_ahorcado(palabra_secreta, False)
     # FIN
-    return
 
 
 def es_solucion_candidata(solucion_como_cadena, candidato):
@@ -103,8 +195,29 @@ def es_solucion_candidata(solucion_como_cadena, candidato):
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
 
+    solucion_como_cadena = solucion_como_cadena.replace(" ", "")
+
+    if len(solucion_como_cadena) != len(candidato):
+        return False
+
+    caracteres_revelados = []
+    for letra in solucion_como_cadena:
+        if letra != "_":
+            caracteres_revelados.append(letra)
+
+    for indice in range(len(solucion_como_cadena)):
+        caracter_solucion = solucion_como_cadena[indice]
+        caracter_candidato = candidato[indice]
+
+        if caracter_solucion != "_" and caracter_solucion != caracter_candidato:
+            return False
+
+        if caracter_solucion == "_" and caracter_candidato in caracteres_revelados:
+            return False
+
+    return True
+
     # FIN
-    return
 
 
 def mostrar_candidatos(solucion_como_cadena):
@@ -120,8 +233,14 @@ def mostrar_candidatos(solucion_como_cadena):
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
 
+    candidatos = []
+    for pais in diccionario:
+        if es_solucion_candidata(solucion_como_cadena, pais.lower()):
+            candidatos.append(pais)
+
+    print(" ".join(candidatos))
+
     # FIN
-    return
 
 
 def iniciar_con_ayuda(palabra_secreta):
@@ -136,14 +255,12 @@ def iniciar_con_ayuda(palabra_secreta):
     """
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
-
+    iniciar_ahorcado(palabra_secreta, True)
     # FIN
     return
 
 
 if __name__ == "__main__":
     palabra_secreta = seleccionar_palabra(diccionario)
-    iniciar(palabra_secreta)
+    iniciar_con_ayuda(palabra_secreta)
 
-    # Para iniciar ahorcado con ayuda, ejecutar
-    # iniciar_con_ayuda(palabra_secreta)
